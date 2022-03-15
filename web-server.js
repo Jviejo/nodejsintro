bc518e73661029fff24cb3b3f03eda55c4ca07f6
golfchain <90685533+golfchain@users.noreply.github.com>
2022-03-15 14:50:54 +0100

    web server

--- web-server.js
+++ web-server.js
@@ -6,6 +6,6 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+app.listen(4444, () => {
+    console.log('Server running on port 4444');
 });
